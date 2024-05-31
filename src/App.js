5315c6fe579c758cdd02f9e35281c4c99c8c2d95
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2024-05-31 13:13:10 -0500

    fix attempt 1 background/ design error occuring on vercel

--- src/App.js
+++ src/App.js
@@ -3,33 +3,29 @@ import styled, { createGlobalStyle } from "styled-components";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import GlobalStyle from "./components/GlobalStyles";
 import IMG_2916 from "./photos/IMG_2916.jpg"; // Import the image
 
 // Global styles using styled-components
-const GlobalStyles = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   /* CSS Reset */
   html,
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-
-  /* Additional Styles */
-  body {
+    width: 100%;
+    height: 100%;
     overflow-x: hidden; /* Prevent horizontal overflow */
-    padding: 0 20px; /* Add padding to the left and right */
-    max-width: auto; /* Optionally set a max-width */
-
-    /* Hide scrollbar for Webkit-based browsers */
-    ::-webkit-scrollbar {
-      display: none;
-    }
-
-    /* Hide scrollbar for Firefox */
-    scrollbar-width: none;
+  
   }
+
+  /* Hide scrollbar for Webkit-based browsers */
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for Firefox */
+  scrollbar-width: none;
 `;
 
 // Container styled-component
@@ -38,7 +34,7 @@ const Container = styled.div`
   background-size: cover; /* Ensure the background image covers the entire container */
   background-position: center; /* Center the background image */
   background-repeat: no-repeat; /* Do not repeat the background image */
-  height: 100vh;
+  min-height: 100vh;
   width: 100%;
   scroll-behavior: smooth;
   overflow-y: auto;
@@ -55,6 +51,7 @@ const Container = styled.div`
 // Content styled-component to add margin
 const Content = styled.div`
   margin: 0 40px; /* Add margin to the left and right */
+
 `;
 
 function App() {
